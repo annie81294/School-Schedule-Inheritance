bd094b7542422dec2ec07f12ac8111910b3f2506
Annie Chang <chun@Annie-Chang-2.local>
2026-09-23 09:26:03 -0700

    All tasks passed

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,9 @@
 from .student import Student
 
-# add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation=False):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+
+    def summary(self):
+        return f"{super().summary()} Transportation: {self.gets_transportation}"
